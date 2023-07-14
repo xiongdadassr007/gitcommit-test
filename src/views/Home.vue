e21196c9848febd072269da309f4e5c9a68a9772
xiongguangsen02 <3096429133@qq.com>
2023-07-14 11:21:44 +0800

    修改上次暂存提交描述04

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: xiongguangsen02 3096429133@qq.com
+ * @Date: 2023-07-13 17:04:19
+ * @LastEditors: xiongguangsen02 3096429133@qq.com
+ * @LastEditTime: 2023-07-13 17:37:08
+ * @FilePath: \新测试用\test-git\src\views\Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
@@ -13,6 +21,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    
   }
 }
 </script>
