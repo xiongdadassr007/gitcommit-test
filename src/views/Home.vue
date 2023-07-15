2797645af70a769a1ecd885d8596fc8af5b8496b
xiongguangsen02 <3096429133@qq.com>
2023-07-15 19:21:59 +0800

    测试001提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-07-13 17:04:19
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-07-13 17:37:08
+ * @LastEditTime: 2023-07-15 19:20:21
  * @FilePath: \新测试用\test-git\src\views\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -23,7 +23,9 @@ export default {
     HelloWorld
   },
   methods: {
-    
+    bsat() {
+      
+    }
   }
 }
 </script>
