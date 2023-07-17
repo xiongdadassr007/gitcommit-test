7d5605b4daaf722921a305cb5eeae9d22c54e9e2
xiongguangsen02 <3096429133@qq.com>
2023-07-17 15:57:43 +0800

    测试提交是否存在合并001

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,12 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-07-13 17:04:19
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-07-15 19:05:25
+ * @LastEditTime: 2023-07-17 15:55:52
  * @FilePath: \新测试用\test-git\src\views\About.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="about">
-    <h1>This is an about page23445"测试"</h1>
+    <h1>This is an about page23445"测试1244"</h1>
   </div>
 </template>
