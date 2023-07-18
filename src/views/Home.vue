7989db9ad1843b60814919488cb9e95f640f0943
xiongguangsen02 <3096429133@qq.com>
2023-07-18 17:37:43 +0800

    测试新建分支后，修改主分支003

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-07-13 17:04:19
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-07-18 16:15:37
+ * @LastEditTime: 2023-07-18 17:37:35
  * @FilePath: \新测试用\test-git\src\views\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,6 +13,7 @@
     <h1>第一次修改主分支</h1>
     <h1>新建分支后，修改主分支代码001</h1>
     <h1>新建分支后，修改主分支代码002</h1>
+    <h1>新建分支后，修改主分支代码003</h1>
   </div>
 </template>
 
