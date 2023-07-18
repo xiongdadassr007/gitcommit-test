49c295d54de318fad59be1c734e948bf4a75e2e2
xiongguangsen02 <3096429133@qq.com>
2023-07-18 17:43:02 +0800

    测试合并02、03、04分支

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: xiongguangsen02 3096429133@qq.com
  * @Date: 2023-07-13 17:04:19
  * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-07-18 16:12:10
+ * @LastEditTime: 2023-07-18 17:55:45
  * @FilePath: \新测试用\test-git\src\views\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -11,6 +11,8 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>第一次修改主分支</h1>
+    <h1>feature-branch 第三次提交</h1>
+    <h1>feature-branch 第四次提交</h1>
   </div>
 </template>
 
